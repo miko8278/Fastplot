a086ed399030d0e6224567a060aa58ea9e05fdf6
Michael Kolorz <miko8278@bht-berlin.de>
2023-12-18 18:42:49 +0100

    Fuege erstes Beispiel hinzu und eine .gitignore

---
+++ fastplot.c
@@ -0,0 +1,31 @@
+#include "pbPlots.h"
+#include "supportLib.h"
+
+int main(){
+	double xs [] = {-2, -1, 0, 1, 2};
+	double ys [] = {2, -1, -2, -1, 2};
+	_Bool success;
+
+	StartArenaAllocator();
+
+	RGBABitmapImageReference *canvasReference = CreateRGBABitmapImageReference();
+	StringReference *errorMessage = CreateStringReference(L"", 0);
+	success = DrawScatterPlot(canvasReference, 2048, 1500, xs, 5, ys, 5, errorMessage);
+
+	if(success){
+		size_t length;
+		ByteArray *pngdata = ConvertToPNG(canvasReference->image);
+		WriteToFile(pngdata, "example1.png");
+		DeleteImage(canvasReference->image);
+	}else{
+		fprintf(stderr, "Error: ");
+		for(int i = 0; i < errorMessage->stringLength; i++){
+			fprintf(stderr, "%c", errorMessage->string[i]);
+		}
+		fprintf(stderr, "\n");
+	}
+
+	FreeAllocations();
+
+	return success ? 0 : 1;
+}
